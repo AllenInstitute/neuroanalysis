26e68292fa49afe252ec624193ff2ac11fd3d998
Megan Kratz <meganbkratz@gmail.com>
2019-12-03 17:17:24 -0700

    bugfix

--- model_test.py
+++ model_test.py
@@ -9,7 +9,7 @@ f2 = "/Users/meganbkratz/Code/ai/example_data/2019_06_24_131623-compressed.nwb"
 
 #hdf = h5py.File(f, 'r')
 
-mies_nwb = Dataset(loader_class=OptoNwbLoader, file_path=f2)
-opto_nwb = Dataset(loader_class=OptoNwbLoader, file_path=f)
+mies_nwb = Dataset(loader_class=MiesNwbLoader, file_path=f2)
+opto_nwb = Dataset(loader_class=MiesNwbLoader, file_path=f)
 
 #old = OptoNwb(f)
