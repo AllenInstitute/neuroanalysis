056f4218ac474ad9e0eda0586adf407f72d99ec5
Megan Kratz <meganbkratz@gmail.com>
2019-12-03 17:06:54 -0700

    Adjustments for using Dataset directly with a loader class (instead of subclassing)
    (cherry picked from commit 818c1e488eb20022c390845696e4efdbb5ad9fb7)

---
+++ model_test.py
@@ -0,0 +1,15 @@
+import os
+import h5py
+from optoanalysis.data.new_dataset import OptoNwbLoader
+from neuroanalysis.data.dataset import Dataset
+from optoanalysis.data.dataset import OptoNwb
+
+f = "/Users/meganbkratz/Code/ai/example_data/data/2019-06-13_000/slice_000/site_000/2019_06_13_exp1_TH-compressed.nwb"
+f2 = "/Users/meganbkratz/Code/ai/example_data/2019_06_24_131623-compressed.nwb"
+
+#hdf = h5py.File(f, 'r')
+
+mies_nwb = Dataset(loader_class=OptoNwbLoader, file_path=f2)
+opto_nwb = Dataset(loader_class=OptoNwbLoader, file_path=f)
+
+#old = OptoNwb(f)
